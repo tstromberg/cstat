b3053d23e4123df9744b2702e49a3e813f15010d
Thomas Stromberg <tstromberg@google.com>
2020-04-13 07:43:47 -0700

    show final average

--- cstat.go
+++ cstat.go
@@ -4,54 +4,88 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"os/signal"
+	"syscall"
 	"time"
 
 	"github.com/lufia/iostat"
 )
 
-var duration = flag.Duration("total", 365*24*time.Hour, "How long to wait until exiting")
+var duration = flag.Duration("for", 365*24*time.Hour, "How long to wait until exiting")
 var wait = flag.Duration("wait", 1*time.Second, "How often to poll")
-var header = flag.Bool("header", true, "show header")
+var showHeader = flag.Bool("header", true, "show header")
 var justBusy = flag.Bool("busy", false, "just show busy score")
+var showTotal = flag.Bool("total", true, "show total at end")
 
 func main() {
 	flag.Parse()
 
-	if *header {
-		fmt.Printf("elapsed\tbusy%%\tsys%%\tuser%%\tnice%%\tidle%%\n")
-	}
+	header()
 	start := time.Now()
-	pst, err := iostat.ReadCPUStats()
+	lastSample := start
+	sst, err := iostat.ReadCPUStats()
 	if err != nil {
 		panic(err)
 	}
 
+	pst := sst
+	sigs := make(chan os.Signal, 1)
+	done := make(chan bool, 1)
+	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
+
+	go func() {
+		<-sigs
+		total(sst, pst, start, lastSample)
+		os.Exit(0)
+		done <- true
+	}()
+
 	for {
 		if time.Since(start) > *duration {
+			total(sst, pst, start, lastSample)
 			os.Exit(0)
 		}
 		time.Sleep(*wait)
+
 		st, err := iostat.ReadCPUStats()
 		if err != nil {
 			panic(err)
 		}
-		t := time.Now()
-		idle := st.Idle - pst.Idle
-		total := (st.User + st.Nice + st.Sys + st.Idle) - (pst.User + pst.Nice + pst.Sys + pst.Idle)
-		busy := total - idle
-
-		if *justBusy {
-			fmt.Printf("%.3f\n", float64(busy)/float64(total)*100)
-		} else {
-			fmt.Printf("%d\t%.3f\t%.3f\t%.3f\t%.3f\t%.3f\n",
-				int64(t.Sub(start).Milliseconds())/1000,
-				float64(busy)/float64(total)*100,
-				float64(st.Sys-pst.Sys)/float64(total)*100,
-				float64(st.User-pst.User)/float64(total)*100,
-				float64(st.Nice-pst.Nice)/float64(total)*100,
-				float64(st.Idle-pst.Idle)/float64(total)*100,
-			)
-		}
+		lastSample = time.Now()
+		display(pst, st, start, lastSample)
 		pst = st
+	}
+}
+
+func header() {
+	if *showHeader {
+		fmt.Printf("elapsed\tbusy%%\tsys%%\tuser%%\tnice%%\tidle%%\n")
+	}
+}
+
+func display(pst *iostat.CPUStats, st *iostat.CPUStats, start time.Time, last time.Time) {
+	idle := st.Idle - pst.Idle
+	total := (st.User + st.Nice + st.Sys + st.Idle) - (pst.User + pst.Nice + pst.Sys + pst.Idle)
+	busy := total - idle
+
+	if *justBusy {
+		fmt.Printf("%.3f\n", float64(busy)/float64(total)*100)
+	} else {
+		fmt.Printf("%d\t%.3f\t%.3f\t%.3f\t%.3f\t%.3f\n",
+			int64(last.Sub(start).Milliseconds())/1000,
+			float64(busy)/float64(total)*100,
+			float64(st.Sys-pst.Sys)/float64(total)*100,
+			float64(st.User-pst.User)/float64(total)*100,
+			float64(st.Nice-pst.Nice)/float64(total)*100,
+			float64(st.Idle-pst.Idle)/float64(total)*100,
+		)
+	}
+}
+
+func total(pst *iostat.CPUStats, st *iostat.CPUStats, start time.Time, last time.Time) {
+	if *showTotal {
+		fmt.Printf("\n\nmeasured average over %s\n", last.Sub(start))
+		header()
+		display(pst, st, start, last)
 	}
 }
